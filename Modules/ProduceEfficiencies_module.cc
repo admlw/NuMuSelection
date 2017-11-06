33ef833926aad9a0e1fae9fbd1e6029b6dfc310f
Adam <a.lister1@lancaster.ac.uk>
2017-11-05 18:31:27 -0600

    added cc0Pi Efficiency

--- Modules/ProduceEfficiencies_module.cc
+++ Modules/ProduceEfficiencies_module.cc
@@ -25,6 +25,7 @@
 #include "art/Framework/Services/Optional/TFileService.h"
 
 // LArSoft Includes
+#include "nusimdata/SimulationBase/MCTruth.h"
 #include "nusimdata/SimulationBase/MCNeutrino.h"
 #include "nusimdata/SimulationBase/MCParticle.h"
 
@@ -66,46 +67,112 @@ class ProduceEfficiencies : public art::EDAnalyzer {
 
     // vars
     bool isEventPassed = false;
+    bool isCC0pi = true;
     double mcNuEnergy;
+    int mcNuCCNC;
+
+    double vx, vy, vz;
 
     // Efficiency histograms
     TEfficiency* mcNuEnergyEff;
+    TEfficiency* mcNuEnergyCC0PiEff;
 };
 
 
 ProduceEfficiencies::ProduceEfficiencies(fhicl::ParameterSet const & p)
   :
     EDAnalyzer(p)  // ,
-    // More initializers here.
-{}
+{
+
+
+}
 
 void ProduceEfficiencies::analyze(art::Event const & e)
 {
 
   // get MC neutrino
-  art::Handle< std::vector<simb::MCNeutrino> > mcNuHandle;
-  e.getByLabel("generator", mcNuHandle);
-  if (!mcNuHandle.isValid()) return;
-  std::vector< art::Ptr<simb::MCNeutrino> > mcNuVec;
-  art::fill_ptr_vector(mcNuVec, mcNuHandle);
+  art::Handle< std::vector<simb::MCTruth> > mcTruthHandle;
+  e.getByLabel("generator", mcTruthHandle);
+  if (!mcTruthHandle.isValid()) return;
+  std::vector< art::Ptr<simb::MCTruth> > mcTruthVec;
+  art::fill_ptr_vector(mcTruthVec, mcTruthHandle);
 
   art::Handle< std::vector<ubana::SelectionResult> > selectionHandle;
   e.getByLabel("UBXSec", selectionHandle);
   if (!selectionHandle.isValid()){
 
+    std::cout << " >> SelectionResult product not found. " << std::endl;
     mf::LogError(__PRETTY_FUNCTION__) << "SelectionResult product not found."
       << std::endl;
     throw std::exception(); 
 
   }
 
-  // get true neutrino energy
-  if (mcNuVec.size() == 0) return;
-  art::Ptr<simb::MCNeutrino> mcNu = mcNuVec.at(0);
+  if (mcTruthVec.size() == 0){ 
+    std::cout << " >> No Neutrinos " << std::endl;
+    return;
+  }
 
-  mcNuEnergy = mcNu->Nu().E();
+  art::Ptr<simb::MCTruth> mcTruth = mcTruthVec.at(0);
+  const simb::MCNeutrino& mcNu = mcTruth->GetNeutrino();
+  const simb::MCParticle& mcNuP = mcNu.Nu();
 
+  vx = mcNuP.Vx();
+  vy = mcNuP.Vy();
+  vz = mcNuP.Vz();
+  mcNuCCNC   = mcNu.CCNC();
+  
+  mcNuEnergy = mcNuP.E();
+
+  // get true numu CC interactions in FV
+  if (mcTruth->Origin() != simb::kBeamNeutrino){
+
+    std::cout << " Not a beam neutrino! " << std::endl; 
+    return;
+
+  }
+
+  if (){
+
+    std::cout << "interaction vertex " << vx << ", " << vy << ", " << vz
+      << "is out of TPC! " << std::endl;
+    return;
+  
+  }
+
+  if (mcNuCCNC != 0){
+  
+    std::cout << " >> Event is NC " << std::endl;
+    return;
+  
+  }
+  if (mcNuP.PdgCode() != 14){
+
+    std::cout << " >> not a nu_mu interaction!" << std::endl;
+    return;
+
+  }
+
+  // check if selected and fill efficiency histogram
   for (auto const& selectionStatus : (*selectionHandle)){
+
+    std::cout << " >> Found a true CC Event " << std::endl;
+    std::cout << " >> Printing daughter particles: " << std::endl; 
+   
+    int nParticles = mcTruth->NParticles();
+    for(int i = 0; i < nParticles; i++){
+      
+        const simb::MCParticle& particle = mcTruth->GetParticle(i);
+        if (particle.Process() != "primary" || particle.StatusCode() != 1) continue;    
+        
+        if (std::abs(particle.PdgCode()) == 211 || std::abs(particle.PdgCode()) == 111){
+
+          isCC0pi = false;
+
+        }
+        else isCC0pi = true;
+
+    }
 
     if (selectionStatus.GetSelectionStatus()){
 
@@ -113,11 +180,21 @@ void ProduceEfficiencies::analyze(art::Event const & e)
       isEventPassed = true;
 
       mcNuEnergyEff->Fill(isEventPassed, mcNuEnergy);
+      std::cout << " -->> Selected." << std::endl;
+
+      if (isCC0pi == true)
+        mcNuEnergyCC0PiEff->Fill(isEventPassed, mcNuEnergy);
 
     }
     else {
-    
-      // event fails
+      // event does not pass
+      isEventPassed = false;
+
+      mcNuEnergyEff->Fill(isEventPassed, mcNuEnergy);
+      std::cout << " -->>Not Selected." << std::endl;
+     
+      if (isCC0pi == true)
+        mcNuEnergyCC0PiEff->Fill(isEventPassed, mcNuEnergy);
 
     }
 
@@ -129,13 +206,15 @@ void ProduceEfficiencies::beginJob()
 {
   // Implementation of optional member function here.
   selectionEfficiency = tfs->make<TTree>("selectionEfficiency", "selectionEfficiency");
-  mcNuEnergyEff = tfs->make<TEfficiency>("mcNuEnergyEff", ";#nu_{E}^{true}; #epsilon", 15, 0, 3);
+  mcNuEnergyEff = tfs->make<TEfficiency>("mcNuEnergyEff", ";E_{#nu}^{true}; #epsilon", 15, 0, 3);
+  mcNuEnergyCC0PiEff = tfs->make<TEfficiency>("mcNuEnergyCC0PiEff", ";E_{#nu}^{true}; #epsilon", 15, 0, 3);
 
 }
 
 void ProduceEfficiencies::endJob()
 {
-  // Implementation of optional member function here.
+
+
 }
 
 DEFINE_ART_MODULE(ProduceEfficiencies)
