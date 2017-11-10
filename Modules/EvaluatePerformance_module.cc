c3bc25ccc828bfc1a4bb77a4fc0bd87231f9821c
Adam <a.lister1@lancaster.ac.uk>
2017-11-09 22:36:27 -0600

    adding purity TEfficiencies and length histograms for signal and backgrounds

--- Modules/EvaluatePerformance_module.cc
+++ Modules/EvaluatePerformance_module.cc
@@ -35,6 +35,7 @@
 #include "TTree.h"
 #include "TEfficiency.h"
 #include "TLorentzVector.h"
+#include "TH1.h"
 
 // UBXSec Includes
 #include "uboone/UBXSec/DataTypes/SelectionResult.h"
@@ -73,12 +74,17 @@ class EvaluatePerformance : public art::EDAnalyzer {
     ubana::FiducialVolume fiducialVolume;  
 
     // vars
-    bool isEventPassed = false;
-    bool isCC0pi = true;
+    bool isEventPassed;
+    bool isCC0pi;
     bool isBeamNeutrino;
-    bool isRecoInTPC;
+    bool isCosmic;
+    bool isMixed;
+    bool isRecoInFV;
     bool isCC;
     bool isMuonNeutrino;
+    bool isMuonAntineutrino;
+    bool isElectronNeutrino;
+    bool isElectronAntineutrino;
     bool isInFV;
     int mcNuCCNC;
 
@@ -88,6 +94,10 @@ class EvaluatePerformance : public art::EDAnalyzer {
     double mcLeptonMom;
     double mcLeptonTheta;
     double mcLeptonPhi;
+
+    double muonCandidateLength;
+
+    std::vector<bool> purityVector = {0,0,0,0,0,0,0,0};
 
     // Efficiency histograms
     TEfficiency* mcNuEnergyEff;
@@ -99,6 +109,28 @@ class EvaluatePerformance : public art::EDAnalyzer {
     TEfficiency* mcLeptonMomCC0PiEff;
     TEfficiency* mcLeptonThetaCC0PiEff;
     TEfficiency* mcLeptonPhiCC0PiEff;
+
+    // Purity histograms
+    TEfficiency* mcNuEnergyPur;
+    TEfficiency* mcLeptonMomPur;
+    TEfficiency* mcLeptonThetaPur;
+    TEfficiency* mcLeptonPhiPur;
+
+    TEfficiency* mcNuEnergyCC0PiPur;
+    TEfficiency* mcLeptonMomCC0PiPur;
+    TEfficiency* mcLeptonThetaCC0PiPur;
+    TEfficiency* mcLeptonPhiCC0PiPur;
+
+    // length histograms
+    TH1D* selectedTrackLengthTrueCC;
+    TH1D* selectedTrackLengthTrueCC0Pi;
+    TH1D* selectedTrackLengthCosmic;
+    TH1D* selectedTrackLengthMixed;
+    TH1D* selectedTrackLengthOutOfFV;
+    TH1D* selectedTrackLengthAntiNuMu;
+    TH1D* selectedTrackLengthNuE;
+    TH1D* selectedTrackLengthAntiNuE;
+    TH1D* selectedTrackLengthNC;
 
 };
 
@@ -187,11 +219,19 @@ void EvaluatePerformance::analyze(art::Event const & e)
   }
   else isMuonNeutrino = true;
 
+  if (mcNuP.PdgCode() == -14) isMuonAntineutrino = true;
+  else isMuonAntineutrino = false;
+
+  if (mcNuP.PdgCode() == 12) isElectronNeutrino = true;
+  else isElectronNeutrino = false;
+
+  if (mcNuP.PdgCode() == -12) isElectronAntineutrino = true;
+  else isElectronAntineutrino = false;
 
   // is true nu interaction in FV
   if (!fiducialVolume.InFV(vx, vy, vz)){
 
-    std::cout << "true interaction vertex " << vx << ", " << vy << ", " << vz
+    std::cout << " >> true interaction vertex " << vx << ", " << vy << ", " << vz
       << "is out of TPC! " << std::endl;
     isInFV = false;
 
@@ -201,12 +241,26 @@ void EvaluatePerformance::analyze(art::Event const & e)
   // ---------------------
   // Reco-level cuts
   // ---------------------
-  std::cout << selectedTpcObjects.at(0).size() << std::endl;
+
   if (selectedTpcObjects.at(0).size() != 0){
     selectedTpcObject = selectedTpcObjects.at(0).at(0);
 
     const recob::Vertex& selectedVertex = selectedTpcObject->GetVertex();
     const ubana::TPCObjectOrigin& selectedOrigin = selectedTpcObject->GetOrigin();
+    const std::vector<recob::Track>& selectedTracks = selectedTpcObject->GetTracks();
+
+    // get candidate muon vars
+    muonCandidateLength = 0;
+    for (size_t i = 0; i < selectedTracks.size(); i++){
+
+      const recob::Track& track = selectedTracks.at(i);
+      if (track.Length() > muonCandidateLength){
+
+        muonCandidateLength = track.Length();
+
+      }
+
+    }
 
     // get reconstructed vertex position
     // if true vertex is in the fiducial volume but reconstructed is out, this goes in the denominator
@@ -214,30 +268,44 @@ void EvaluatePerformance::analyze(art::Event const & e)
     selectedVertex.XYZ(xyz);
     if (fiducialVolume.InFV(xyz[0], xyz[1], xyz[2]) == true){
 
-      std::cout << "Reconstruced object is in TPC" << std::endl;
-      isRecoInTPC = true;
+      std::cout << " >> Reconstruced object is in TPC" << std::endl;
+      isRecoInFV = true;
 
     }
     else{
-      std::cout << "Reconstructed object is out of TPC!" << std::endl;
-      isRecoInTPC = false;
+      std::cout << " >> Reconstructed object is out of TPC!" << std::endl;
+      isRecoInFV = false;
     }
 
 
     // get selected TPCObject  origin
     if ( selectedOrigin == ubana::kBeamNeutrino){
 
-      std::cout << "Found a beam neutrino! " << std::endl; 
+      std::cout << " >> Found a beam neutrino! " << std::endl; 
       isBeamNeutrino = true;
+      isCosmic = false;
+      isMixed = false;
 
     }
-    else{
+    else if (selectedOrigin == ubana::kCosmicRay) {
 
-      std::cout << "Not a beam neutrino!" << std::endl;
+      std::cout << " >> Not a beam neutrino!" << std::endl;
       isBeamNeutrino = false;
+      isCosmic = true;
+      isMixed = false;
 
     }
+    else if (selectedOrigin == ubana::kMixed){
+
+      std::cout << " >> Not a beam neutrino!" << std::endl;
+      isBeamNeutrino = false;
+      isCosmic = false;
+      isMixed = true;
+
+    }
+    else isBeamNeutrino = false;
   }
+
   // check if selected and fill efficiency histogram
   for (auto const& selectionStatus : (*selectionHandle)){
 
@@ -258,28 +326,90 @@ void EvaluatePerformance::analyze(art::Event const & e)
         else isCC0pi = true;
 
       }
+    }
 
-      if (selectionStatus.GetSelectionStatus() == true && isBeamNeutrino == true && isRecoInTPC == true){
+    if (selectionStatus.GetSelectionStatus() == true){
 
+      if (isBeamNeutrino == true && isRecoInFV == true && isCC == true && isMuonNeutrino == true && isInFV == true){
         // event passes
+
+        std::cout << " -->> Selected." << std::endl;
         isEventPassed = true;
 
         mcNuEnergyEff->Fill(isEventPassed, mcNuEnergy);
         mcLeptonMomEff->Fill(isEventPassed, mcLeptonMom);
         mcLeptonThetaEff->Fill(isEventPassed, mcLeptonTheta);
         mcLeptonPhiEff->Fill(isEventPassed, mcLeptonPhi);
-        std::cout << " -->> Selected." << std::endl;
+        
+        // purity
+        
+        mcNuEnergyPur->Fill(true, mcNuEnergy);
+        mcLeptonMomPur->Fill(true, mcLeptonMom);
+        mcLeptonThetaPur->Fill(true, mcLeptonTheta);
+        mcLeptonPhiPur->Fill(true, mcLeptonPhi);
+
+        selectedTrackLengthTrueCC->Fill(muonCandidateLength);
 
         if (isCC0pi == true){
+
           mcNuEnergyCC0PiEff->Fill(isEventPassed, mcNuEnergy);
           mcLeptonMomCC0PiEff->Fill(isEventPassed, mcLeptonMom);
           mcLeptonThetaCC0PiEff->Fill(isEventPassed, mcLeptonTheta);
           mcLeptonPhiCC0PiEff->Fill(isEventPassed, mcLeptonPhi);
+        
+          mcNuEnergyCC0PiPur->Fill(true, mcNuEnergy);
+          mcLeptonMomCC0PiPur->Fill(true, mcLeptonMom);
+          mcLeptonThetaCC0PiPur->Fill(true, mcLeptonTheta);
+          mcLeptonPhiCC0PiPur->Fill(true, mcLeptonPhi);
+
         }
+
       }
-      else {
-        // event does not pass
-        isEventPassed = false;
+      else{ 
+        std::cout << "selected event is not signal!" << std::endl;
+
+        purityVector.at(0) = isCosmic;
+        purityVector.at(1) = isMixed;
+        purityVector.at(2) = !(isInFV && !isRecoInFV);
+        purityVector.at(3) = isMuonAntineutrino;
+        purityVector.at(4) = isElectronNeutrino;
+        purityVector.at(5) = isElectronAntineutrino;
+        purityVector.at(6) = !isCC;
+
+        // purity 
+        
+        mcNuEnergyPur->Fill(false, mcNuEnergy);
+        mcLeptonMomPur->Fill(false, mcLeptonMom);
+        mcLeptonThetaPur->Fill(false, mcLeptonTheta);
+        mcLeptonPhiPur->Fill(false, mcLeptonPhi);
+
+        mcNuEnergyCC0PiPur->Fill(false, mcNuEnergy);
+        mcLeptonMomCC0PiPur->Fill(false, mcLeptonMom);
+        mcLeptonThetaCC0PiPur->Fill(false, mcLeptonTheta);
+        mcLeptonPhiCC0PiPur->Fill(false, mcLeptonPhi);
+
+        for ( size_t i = 0; i < purityVector.size(); i++ ){
+
+          std::cout << " >> " << purityVector.at(i) << std::endl;;
+
+        }
+
+        if (isCosmic){ selectedTrackLengthCosmic->Fill(muonCandidateLength); continue;}
+        if (isMixed){ selectedTrackLengthMixed->Fill(muonCandidateLength); continue;}
+        if (!(isInFV && !isRecoInFV)){ selectedTrackLengthOutOfFV->Fill(muonCandidateLength); continue;}
+        if (isMuonAntineutrino){ selectedTrackLengthAntiNuMu->Fill(muonCandidateLength); continue;}
+        if (isElectronNeutrino){ selectedTrackLengthNuE->Fill(muonCandidateLength); continue;}
+        if (isElectronAntineutrino){ selectedTrackLengthAntiNuE->Fill(muonCandidateLength); continue;}
+        if (isCC){ selectedTrackLengthNC->Fill(muonCandidateLength); continue;}
+
+      }
+
+    }
+    else {
+
+      // event does not pass
+      isEventPassed = false;
+      if (isBeamNeutrino == true && isRecoInFV == true && isCC == true && isMuonNeutrino == true && isInFV == true){
 
         mcNuEnergyEff->Fill(isEventPassed, mcNuEnergy);
         mcLeptonMomEff->Fill(isEventPassed, mcLeptonMom);
@@ -297,20 +427,49 @@ void EvaluatePerformance::analyze(art::Event const & e)
       }
     }
   }
+
+  selectionEfficiency->Fill();
 }
 
 void EvaluatePerformance::beginJob()
 {
   // Implementation of optional member function here.
   selectionEfficiency = tfs->make<TTree>("selectionEfficiency", "selectionEfficiency");
+  selectionEfficiency->Branch("purityVector", &purityVector, "purityVector");  
+
+  // efficiencies
+
   mcNuEnergyEff = tfs->make<TEfficiency>("mcNuEnergyEff", ";E_{#nu}^{true}; #epsilon", 15, 0, 3);
   mcLeptonMomEff = tfs->make<TEfficiency>("mcLeptonMomEff", ";P_{l}^{true}; #epsilon", 15, 0, 2);
-  mcLeptonThetaEff = tfs->make<TEfficiency>("mcLeptonThetaEff", ";#theta_{l}^{true}; #epsilon;", 10, -1, 1);
+  mcLeptonThetaEff = tfs->make<TEfficiency>("mcLeptonThetaEff", ";cos(#theta_{l}^{true}); #epsilon;", 10, -1, 1);
   mcLeptonPhiEff = tfs->make<TEfficiency>("mcLeptonPhiEff", ";#phi_{l}^{true}; #epsilon", 15, -3, 3);
   mcNuEnergyCC0PiEff = tfs->make<TEfficiency>("mcNuEnergyCC0PiEff", ";E_{#nu}^{true}; #epsilon", 15, 0, 3);
   mcLeptonMomCC0PiEff = tfs->make<TEfficiency>("mcLeptonMomCC0PiEff", ";P_{l}^{true}; #epsilon", 15, 0, 2);
-  mcLeptonThetaCC0PiEff = tfs->make<TEfficiency>("mcLeptonThetaCC0PiEff", ";#theta_{l}^{true}; #epsilon;", 10, -1, 1);
+  mcLeptonThetaCC0PiEff = tfs->make<TEfficiency>("mcLeptonThetaCC0PiEff", ";cos(#theta_{l}^{true}); #epsilon;", 10, -1, 1);
   mcLeptonPhiCC0PiEff = tfs->make<TEfficiency>("mcLeptonPhiCC0PiEff", ";#phi_{l}^{true};#epsilon", 15, -3, 3);
+
+  // purity
+  
+  mcNuEnergyPur = tfs->make<TEfficiency>("mcNuEnergyPur", ";E_{#nu}^{true};p", 15, 0, 3);
+  mcLeptonMomPur = tfs->make<TEfficiency>("mcLeptonMomPur", ";P_{l}^{true};p", 15, 0, 2);
+  mcLeptonThetaPur = tfs->make<TEfficiency>("mcLeptonThetaPur", ";cos(#theta_{l}^{true});p", 10, -1, 1);
+  mcLeptonPhiPur = tfs->make<TEfficiency>("mcLeptonPhiPur", ";#phi_{l}^{true};p", 15, -3, 3);
+
+  mcNuEnergyCC0PiPur = tfs->make<TEfficiency>("mcNuEnergyCC0PiPur", ";E_{#nu}^{true};p", 15, 0, 3);
+  mcLeptonMomCC0PiPur = tfs->make<TEfficiency>("mcLeptonMomCC0PiPur", ";P_{l}^{true};p", 15, 0, 2);
+  mcLeptonThetaCC0PiPur = tfs->make<TEfficiency>("mcLeptonThetaCC0PiPur", ";cos(#theta_{l}^{true});p", 10, -1, 1);
+  mcLeptonPhiCC0PiPur = tfs->make<TEfficiency>("mcLeptonPhiCC0PiPur", ";#phi_{l}^{true};p", 15, -3, 3);
+
+  // length histograms
+  selectedTrackLengthTrueCC = tfs->make<TH1D>("selectedTrackLengthTrueCC", ";Candidate muon length; # tracks", 30, 0, 700);
+  selectedTrackLengthTrueCC0Pi = tfs->make<TH1D>("selectedTrackLengthTrueCC0Pi", ";Candidate muon length; # tracks", 30, 0, 700);
+  selectedTrackLengthCosmic = tfs->make<TH1D>("selectedTrackLengthCosmic", ";Candidate muon length; # tracks", 30, 0, 700);
+  selectedTrackLengthMixed = tfs->make<TH1D>("selectedTrackLengthMixed", ";Candidate muon length; # tracks", 30, 0, 700);
+  selectedTrackLengthOutOfFV = tfs->make<TH1D>("selectedTrackLengthOutOfFV", ";Candidate muon length; # tracks", 30, 0, 700);
+  selectedTrackLengthAntiNuMu = tfs->make<TH1D>("selectedTrackLengthAntiNuMu", ";Candidate muon length; # tracks", 30, 0, 700);
+  selectedTrackLengthNuE = tfs->make<TH1D>("selectedTrackLengthNuE", ";Candidate muon length; # tracks", 30, 0, 700);
+  selectedTrackLengthAntiNuE = tfs->make<TH1D>("selectedTrackLengthAntiNuE", ";Candidate muon length; # tracks", 30, 0, 700);
+  selectedTrackLengthNC = tfs->make<TH1D>("selectedTrackLengthNC", ";Candidate muon length; # tracks", 30, 0, 700);
 
 }
 
