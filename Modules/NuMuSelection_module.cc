c577f98d92bcd94de2069c87d698a11ae46ad2c9
Adam <a.lister1@lancaster.ac.uk>
2017-11-28 15:01:42 -0600

    using pidutility to get average dQdX from hits --- temporary measure

--- Modules/NuMuSelection_module.cc
+++ Modules/NuMuSelection_module.cc
@@ -7,6 +7,7 @@
 // from cetlib version v1_21_00.
 ////////////////////////////////////////////////////////////////////////
 
+// Base Includes
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
@@ -16,129 +17,210 @@
 #include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
+
+// art Includes
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "art/Framework/Services/Optional/TFileService.h"
 #include "art/Framework/Services/Optional/TFileDirectory.h"
+#include "canvas/Persistency/Common/FindManyP.h"
 
+// LArSoft Includes
 #include "nusimdata/SimulationBase/MCTruth.h"
 #include "nusimdata/SimulationBase/MCNeutrino.h"
 #include "nusimdata/SimulationBase/MCParticle.h"
+#include "lardataobj/RecoBase/Track.h"
+#include "lardataobj/RecoBase/Hit.h"
+#include "lardataobj/RawData/RawDigit.h"
 
+// ROOT Includes
 #include "TTree.h"
+
+// UBXSec Includes
+#include "uboone/UBXSec/DataTypes/SelectionResult.h"
+#include "uboone/UBXSec/DataTypes/TPCObject.h"
+#include "uboone/UBXSec/Algorithms/FiducialVolume.h"
+
+// local Includes
+#include "uboone/NuMuSelection/Algos/particleIdUtility.h"
 
 class NuMuSelection;
 
 
 class NuMuSelection : public art::EDAnalyzer {
-    public:
-        explicit NuMuSelection(fhicl::ParameterSet const & p);
-        // The compiler-generated destructor is fine for non-base
-        // classes without bare pointers or other resource use.
+  public:
+    explicit NuMuSelection(fhicl::ParameterSet const & p);
+    // The compiler-generated destructor is fine for non-base
+    // classes without bare pointers or other resource use.
 
-        // Plugins should not be copied or assigned.
-        NuMuSelection(NuMuSelection const &) = delete;
-        NuMuSelection(NuMuSelection &&) = delete;
-        NuMuSelection & operator = (NuMuSelection const &) = delete;
-        NuMuSelection & operator = (NuMuSelection &&) = delete;
+    // Plugins should not be copied or assigned.
+    NuMuSelection(NuMuSelection const &) = delete;
+    NuMuSelection(NuMuSelection &&) = delete;
+    NuMuSelection & operator = (NuMuSelection const &) = delete;
+    NuMuSelection & operator = (NuMuSelection &&) = delete;
 
-        // Required functions.
-        void analyze(art::Event const & e) override;
+    // Required functions.
+    void analyze(art::Event const & e) override;
 
-        // Selected optional functions.
-        void reconfigure(fhicl::ParameterSet const & p) override;
-        void beginJob() override;
-        void endJob() override;
+    // Selected optional functions.
+    void reconfigure(fhicl::ParameterSet const & p) override;
+    void beginJob() override;
+    void endJob() override;
 
-    private:
+  private:
 
-        // fcl pars
-        bool isData;
-        bool isGetTruthTree;
-        std::string fTruthLabel;
-        std::string fParticleLabel;
+    pidutil::particleIdUtility pidutils;
 
-        bool isMc;
+    // fcl pars
+    bool isData;
+    bool isGetTruthTree;
+    std::string fTruthLabel;
+    std::string fParticleLabel;
 
-        int fRun;
-        int fSubRun;
-        int fEvent;
-        
-        TTree *truthTree;
+    bool isMc;
+
+    int fRun;
+    int fSubRun;
+    int fEvent;
+
+    TTree *truthTree;
+
 };
 
 
 NuMuSelection::NuMuSelection(fhicl::ParameterSet const & p)
-    :
-        EDAnalyzer(p)  // ,
-        // More initializers here.
+  :
+    EDAnalyzer(p)  // ,
+    // More initializers here.
 {}
 
 void NuMuSelection::reconfigure(fhicl::ParameterSet const & p)
 {
 
-    isData = p.get< bool > ("IsData");
-    isGetTruthTree = p.get< bool > ("ProduceTruthTree");
-    fTruthLabel = p.get< std::string > ("TruthLabel");
-    fParticleLabel = p.get< std::string > ("MCParticleLabel");
+  isData = p.get< bool > ("IsData");
+  isGetTruthTree = p.get< bool > ("ProduceTruthTree");
+  fTruthLabel = p.get< std::string > ("TruthLabel");
+  fParticleLabel = p.get< std::string > ("MCParticleLabel");
 
 }
 
 void NuMuSelection::beginJob()
 {
 
-    art::ServiceHandle< art::TFileService > tfs;
+  art::ServiceHandle< art::TFileService > tfs;
 
-    truthTree = tfs->make<TTree>("truthTree", "truthTree");
+  truthTree = tfs->make<TTree>("truthTree", "truthTree");
 
 }
 
 void NuMuSelection::analyze(art::Event const & e)
 {
-    fRun = e.run();
-    fSubRun = e.subRun();
-    fEvent = e.event();
 
-    isData = e.isRealData();
-    isMc = !isData;
-    if (isData)
-        std::cout << ">| Running over PHYSICS DATA" << std::endl;
-    else
-        std::cout << ">| Running over SIMULATED DATA" << std::endl;
+  // selection info
+  art::Handle< std::vector<ubana::SelectionResult> > selectionHandle;
+  e.getByLabel("UBXSec", selectionHandle);
+  if (!selectionHandle.isValid()){
 
-    if (isGetTruthTree){
+    std::cout << " >> SelectionResult product not found. " << std::endl;
+    mf::LogError(__PRETTY_FUNCTION__) << "SelectionResult product not found."
+      << std::endl;
+    throw std::exception(); 
 
-        std::cout << "\n>| Producing Truth Tree" << std::endl;
+  }
 
-        art::Handle< std::vector<simb::MCTruth> > truthHandle; // you can't handle the truth
-        e.getByLabel(fTruthLabel, truthHandle);
-        
-        for (auto const& truth : *(truthHandle)){
+  // get Selected TPCObject from selection handle
+  art::FindManyP<ubana::TPCObject> selectedTpcObjects(selectionHandle, e, "UBXSec");
+  art::Ptr<ubana::TPCObject> selectedTpcObject; 
 
-            auto const& trueOrigin = truth.Origin(); // simb::kBeamNeutrino or simb::kCosmicRay
+  // track handle
+  art::Handle< std::vector< recob::Track > > trackHandle;
+  e.getByLabel("pandoraNu", trackHandle);
 
-            if (trueOrigin == simb::kBeamNeutrino){
+  // hit information
+  art::FindManyP< recob::Hit > hitsFromTrack(trackHandle, e, "pandoraNu");
 
-                auto const& nu = truth.GetNeutrino();
-                int nuPdg = nu.Nu().PdgCode(); 
-                int nuCcNc = nu.CCNC();
+  // get RawDigits to pass to dQdX calculation
+  art::Handle< std::vector< raw::RawDigit > > rawDHandle;
+  e.getByLabel("wcNoiseFilter", rawDHandle);
+  std::vector< art::Ptr< raw::RawDigit > > rawDVec;
+  art::fill_ptr_vector(rawDVec, rawDHandle);
 
-                std::cout << ">>|------------- " << std::endl;
-                std::cout << ">>| Found true neutrino with pdg code " << nuPdg << std::endl;
-                std::cout << ">>| Interaction type? (CC = 0, NC = 1)" << nuCcNc << std::endl;
 
-            }
+  fRun = e.run();
+  fSubRun = e.subRun();
+  fEvent = e.event();
 
-        }
+  isData = e.isRealData();
+  isMc = !isData;
+  if (isData)
+    std::cout << ">| Running over PHYSICS DATA" << std::endl;
+  else
+    std::cout << ">| Running over SIMULATED DATA" << std::endl;
 
+  // save truth information to root file. Probably not needed but keeping for now
+  if (isGetTruthTree && isMc){
+
+    std::cout << "\n>| Producing Truth Tree" << std::endl;
+
+    art::Handle< std::vector<simb::MCTruth> > truthHandle;
+    e.getByLabel(fTruthLabel, truthHandle);
+
+    for (auto const& truth : *(truthHandle)){
+
+      auto const& trueOrigin = truth.Origin(); // simb::kBeamNeutrino or simb::kCosmicRay
+
+      if (trueOrigin == simb::kBeamNeutrino){
+
+        auto const& nu = truth.GetNeutrino();
+        int nuPdg = nu.Nu().PdgCode(); 
+        int nuCcNc = nu.CCNC();
+
+        std::cout << ">>|------------- " << std::endl;
+        std::cout << ">>| Found true neutrino with pdg code " << nuPdg << std::endl;
+        std::cout << ">>| Interaction type? (CC = 0, NC = 1)" << nuCcNc << std::endl;
+
+      }
 
     }
 
-    truthTree->Fill();
+  }
+  else std::cout << ">>| You can't handle the truth" << std::endl;
+
+  //
+  // selection begins here
+  //
+  
+  if (selectedTpcObjects.at(0).size() == 1){ 
+
+    selectedTpcObject = selectedTpcObjects.at(0).at(0);
+
+    const std::vector<recob::Track>& selectedTracks = selectedTpcObject->GetTracks();
+
+    for (size_t i = 0; i < selectedTracks.size(); i++){
+
+      const recob::Track& track = selectedTracks.at(i);
+
+      std::vector< art::Ptr< recob::Hit > > hits = hitsFromTrack.at(track.ID());
+
+      std::pair<double, TH1D*> averageDqdx = pidutils.getAveragedQdX(track, hits, rawDVec, true);
+
+      std::cout << averageDqdx.first << std::endl;
+
+    }
+
+  }
+  else {
+
+    std::cout << ">>| Uh-oh, there's more than one selected TPC object, that shouldn't be right..." << std::endl;
+
+  }
+
+
+  truthTree->Fill();
 }
 
 void NuMuSelection::endJob()
 {
-    // Implementation of optional member function here.
+  // Implementation of optional member function here.
 }
 
 DEFINE_ART_MODULE(NuMuSelection)
