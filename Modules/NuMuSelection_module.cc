84846100c3c2e7c2448757c7ccb6d7b7dc62a7b5
Adam <a.lister1@lancaster.ac.uk>
2018-03-22 13:48:00 -0500

    fixing up, adding comments

--- Modules/NuMuSelection_module.cc
+++ Modules/NuMuSelection_module.cc
@@ -39,8 +39,12 @@
 #include "uboone/UBXSec/DataTypes/TPCObject.h"
 #include "uboone/UBXSec/Algorithms/FiducialVolume.h"
 
+// EventWeight includes
+#include "uboone/EventWeight/EventWeightTreeUtility.h"
+
 // local Includes
 #include "uboone/NuMuSelection/Algos/particleIdUtility.h"
+
 
 class NuMuSelection;
 
@@ -112,7 +116,11 @@ void NuMuSelection::produce(art::Event & e)
 
   selResult.SetSelectionStatus(false);
   selResult.SetFailureReason("NoUBXSec");
-  
+ 
+  /**
+   * Module produces a new ubana::SelectionResult, a new ubana::TPCObkect
+   * and an association between the two
+   */
   std::unique_ptr< std::vector<ubana::SelectionResult> > selectionCollection( new std::vector<ubana::SelectionResult> );
   std::unique_ptr< std::vector<ubana::TPCObject> > tpcObjectCollection( new std::vector<ubana::TPCObject> );
   std::unique_ptr< art::Assns<ubana::TPCObject, ubana::SelectionResult> > selTpcObjAssn( new art::Assns<ubana::TPCObject, ubana::SelectionResult>);
@@ -122,7 +130,7 @@ void NuMuSelection::produce(art::Event & e)
   e.getByLabel("UBXSec", selectionHandle);
   if (!selectionHandle.isValid()){
 
-    std::cout << " >> SelectionResult product not found. " << std::endl;
+    std::cout << "[NUMUSEL] SelectionResult product not found. " << std::endl;
     mf::LogError(__PRETTY_FUNCTION__) << "SelectionResult product not found."
       << std::endl;
     throw std::exception(); 
@@ -146,13 +154,13 @@ void NuMuSelection::produce(art::Event & e)
   std::vector< art::Ptr< raw::RawDigit > > rawDVec;
   art::fill_ptr_vector(rawDVec, rawDHandle);
 
-  std::cout << "\n>| PRINTING INFORMATION FOR EVENT " << fRun << "." << fSubRun << "." << fEvent << std::endl;
+  std::cout << "[NUMUSEL] PRINTING INFORMATION FOR EVENT " 
+    << fRun << "." << fSubRun << "." << fEvent << std::endl;
 
-  if (isData)
-    std::cout << ">|>| Running over PHYSICS DATA" << std::endl;
-  else
-    std::cout << ">|>| Running over SIMULATED DATA" << std::endl;
-
+  /**
+   * If there is more than one selected object, then 
+   * move to the next event.
+   */
   if (selectedTpcObjects.at(0).size() == 1){ 
 
     selectedTpcObject = selectedTpcObjects.at(0).at(0);
@@ -162,15 +170,25 @@ void NuMuSelection::produce(art::Event & e)
 
     int muonLikeCounter = 0;
 
+    /**
+     * Loop tracks and check whether each trak is compatibile with
+     * being a muon. If more than one track is muon-like then 
+     * the event does not pass.
+     *
+     * This is currently a stop-gap fix until working PID can be put
+     * here
+     */
     for (size_t i = 0; i < selectedTracks.size(); i++){
 
       const recob::Track& track = selectedTracks.at(i);
 
       std::vector< art::Ptr< recob::Hit > > hits = hitsFromTrack.at(track.ID());
 
-      std::pair<double, double> averageDqdx = pidutils.getAveragedQdX(track, hits, rawDVec, false);
+      std::pair<double, double> averageDqdx = 
+        pidutils.getAveragedQdX(track, hits, rawDVec, false);
 
-      bool isMuLike = pidutils.isMuonLike(averageDqdx.first, averageDqdx.second);
+      bool isMuLike = 
+        pidutils.isMuonLike(averageDqdx.first, averageDqdx.second);
 
       if (isMuLike){ 
         muonLikeCounter++;
@@ -178,7 +196,8 @@ void NuMuSelection::produce(art::Event & e)
 
     }
 
-    std::cout << "\n>|>| Found " << muonLikeCounter << " muon candidates! " << std::endl;
+    std::cout << "[NUMUSEL] Found " 
+      << muonLikeCounter << " muon candidates! " << std::endl;
 
     if (muonLikeCounter == 0){
       selResult.SetSelectionStatus(false);
@@ -193,34 +212,38 @@ void NuMuSelection::produce(art::Event & e)
       selResult.SetFailureReason("Passed");
     }
 
-
     if (nSelectedShowers != 0){
       selResult.SetSelectionStatus(false);
       selResult.SetFailureReason("RecoShower");
     }
 
-
     tpcObjectCollection->push_back(*(selectedTpcObject.get()));
   }
   else if (selectedTpcObjects.at(0).size() == 0){
-
-    std::cout << ">|>| No TPC object selected, on to the next event!" << std::endl;
-
+    std::cout 
+      << "[NUMUSEL] No TPC object selected, on to the next event!" << std::endl;
   }
   else {
-
-    std::cout << ">|>| Uh-oh, there's more than one selected TPC object, that shouldn't be right..." << std::endl;
-
+    std::cout 
+      << "[NUMUSEL] Uh-oh, there's more than one selected TPC object, that shouldn't be right..." << std::endl;
   }
 
-  std::cout << "\n>|>| PRINTING SELECTION INFORMATION" << std::endl;
-  std::cout << ">|>| SELECTION RESULT: " << selResult.GetSelectionStatus() << std::endl;
-  if (selResult.GetSelectionStatus() == 0)
-    std::cout << ">|>| REASON: " << selResult.GetFailureReason() << "\n" << std::endl;
-
+  std::cout << "[NUMUSEL] PRINTING SELECTION INFORMATION" << std::endl;
+  std::cout << "[NUMUSEL] SELECTION RESULT: " 
+    << selResult.GetSelectionStatus() << std::endl;
+  if (selResult.GetSelectionStatus() == 0){
+    std::cout << "[NUMUSEL] REASON: " 
+      << selResult.GetFailureReason() << "\n" << std::endl;
+  }
 
   selectionCollection->push_back(selResult);
-  util::CreateAssn(*this, e, *tpcObjectCollection, *selectionCollection, *selTpcObjAssn, 0, selectionCollection->size());
+  util::CreateAssn(*this, 
+      e, 
+      *tpcObjectCollection, 
+      *selectionCollection, 
+      *selTpcObjAssn, 
+      0, 
+      selectionCollection->size());
 
   e.put(std::move(selectionCollection));
   e.put(std::move(tpcObjectCollection));
