1e99d9c9b0c40a3399d4179cf7feeaabeb04775d
Adam <a.lister1@lancaster.ac.uk>
2018-07-09 21:51:02 -0500

    moving to using an analyzer module, and incorporating new PID tools

--- Modules/NuMuSelection1muNpAnalyzer_module.cc
+++ Modules/NuMuSelection1muNpAnalyzer_module.cc
@@ -78,7 +78,17 @@ class NuMuSelection1muNpAnalyzer : public art::EDAnalyzer {
 
     // User Functions
 
+    /**
+     * Initialises all tree variables for ana_tree
+     */
     void initialiseAnalysisTree(TTree*, bool fIsData);
+
+    /**
+     * Resizes vectors to size 0 at the start of an event
+     */
+    void resizeVectors();
+
+    std::pair< const simb::MCParticle*, double > GetAssociatedMCParticle(std::vector< art::Ptr<recob::Hit> > hits, art::FindMany< simb::MCParticle, anab::BackTrackerHitMatchingData > particlesPerHit);
 
   private:
 
@@ -113,6 +123,28 @@ class NuMuSelection1muNpAnalyzer : public art::EDAnalyzer {
     bool isCosmic;
     int nSelectedTracks;
     int nSelectedShowers;
+    std::vector<double>* noBragg_fwd_mip = nullptr;
+    std::vector<double>* bragg_fwd_mu    = nullptr;
+    std::vector<double>* bragg_fwd_p     = nullptr;
+    std::vector<double>* bragg_fwd_pi    = nullptr;
+    std::vector<double>* bragg_fwd_k     = nullptr;
+    std::vector<double>* bragg_bwd_mu    = nullptr;
+    std::vector<double>* bragg_bwd_p     = nullptr;
+    std::vector<double>* bragg_bwd_pi    = nullptr;
+    std::vector<double>* bragg_bwd_k     = nullptr;
+    std::vector<double>* track_length    = nullptr;
+    std::vector<double>* track_theta     = nullptr;
+    std::vector<double>* track_costheta  = nullptr;
+    std::vector<double>* track_phi       = nullptr;
+    std::vector<double>* track_startx    = nullptr;
+    std::vector<double>* track_starty    = nullptr;
+    std::vector<double>* track_startz    = nullptr;
+    std::vector<double>* track_endx      = nullptr;
+    std::vector<double>* track_endy      = nullptr;
+    std::vector<double>* track_endz      = nullptr;
+    double true_purity;
+    double true_PDG;
+
 };
 
 
@@ -127,7 +159,7 @@ NuMuSelection1muNpAnalyzer::NuMuSelection1muNpAnalyzer(fhicl::ParameterSet const
   fSelectionLabel      = p_labels.get<std::string>("SelectionLabel", "UBXSec");
   fHitLabel            = p_labels.get<std::string>("HitLabel", "pandoraCosmicHitRemoval::UBXSec");
   fTrackLabel          = p_labels.get<std::string>("TrackLabel", "pandoraNu::UBXSec");
-  fPIDLabel            = p_labels.get<std::string>("fPIDLabel");
+  fPIDLabel            = p_labels.get<std::string>("ParticleIdLabel");
 
   fSelectionTPCObjAssn = p_labels.get<std::string>("SelectionTPCObjAssn", "UBXSec");
   fHitTrackAssn        = p_labels.get<std::string>("hitTrackAssn", "pandoraNu::UBXSec");
@@ -172,9 +204,9 @@ void NuMuSelection1muNpAnalyzer::analyze(art::Event const & e)
   art::FindManyP< recob::Hit > hitsFromTrack(trackHandle, e, fHitTrackAssn);
   art::FindManyP< anab::ParticleID > pidFromTrack(trackHandle, e, fPIDLabel);
 
-                                                                                  
+
   // finally, get  MCTruth, MCFlux, and GTruth information for reweighting                      
-  
+
   art::Handle< std::vector< simb::MCTruth > > mcTruthHandle;                          
   std::vector< art::Ptr<simb::MCTruth> > mcTruthVec;                                  
 
@@ -228,6 +260,7 @@ void NuMuSelection1muNpAnalyzer::analyze(art::Event const & e)
   recob::Vertex selectedVertex;
   nSelectedShowers = 0;
   nSelectedTracks = 0;
+  resizeVectors();
 
   int nSelectedTPCObjects = selectedTPCObjects.at(0).size();
 
@@ -266,14 +299,71 @@ void NuMuSelection1muNpAnalyzer::analyze(art::Event const & e)
 
       const recob::Track& track = selectedTracks.at(i);
 
-      std::cout << track.Length() << std::endl;
+      std::vector< art::Ptr< recob::Hit > > hits = hitsFromTrack.at(track.ID());
+      std::vector< art::Ptr< anab::ParticleID > > pids = pidFromTrack.at(track.ID());
 
+      if (pids.size() == 0){
+
+        std::cout << "[NuMuSelection] No track<->PID association found for track with ID " << track.ID() << ". Throwing exception. " << std::endl;
+        throw std::exception();
+
+      }
+
+      // get PID information
+      std::vector< anab::sParticleIDAlgScores > algScoresVec = pids.at(0)->ParticleIDAlgScores();
+
+      for (size_t i_alg = 0; i_alg < algScoresVec.size(); i_alg++){
+
+        anab::sParticleIDAlgScores algScore = algScoresVec.at(i_alg);
+
+        if(algScore.fAlgName == "BraggPeakLLH" && algScore.fPlaneID.Plane == 2){                                   
+          if (anab::kVariableType(algScore.fVariableType) == anab::kLogL_fwd){     
+
+            if (algScore.fAssumedPdg == 13  ) bragg_fwd_mu->push_back(algScore.fValue);   
+            if (algScore.fAssumedPdg == 2212) bragg_fwd_p->push_back(algScore.fValue);   
+            if (algScore.fAssumedPdg == 211 ) bragg_fwd_pi->push_back(algScore.fValue);   
+            if (algScore.fAssumedPdg == 321 ) bragg_fwd_k->push_back(algScore.fValue);   
+            if (algScore.fAssumedPdg == 0   ) noBragg_fwd_mip->push_back(algScore.fValue);   
+
+          }                                                                        
+          else if (anab::kVariableType(algScore.fVariableType) == anab::kLogL_bwd){
+
+            if (algScore.fAssumedPdg == 13  ) bragg_bwd_mu->push_back(algScore.fValue);      
+            if (algScore.fAssumedPdg == 2212) bragg_bwd_p->push_back(algScore.fValue);      
+            if (algScore.fAssumedPdg == 211 ) bragg_bwd_pi->push_back(algScore.fValue);      
+            if (algScore.fAssumedPdg == 321 ) bragg_bwd_k->push_back(algScore.fValue);      
+
+          }                                                                        
+
+        }
+
+      }
+
+      track_length->push_back(track.Length());
+      track_theta->push_back(track.Theta());
+      track_costheta->push_back(std::cos(track.Theta()));
+      track_phi->push_back(track.Phi());
+      track_startx->push_back(track.Start().X());
+      track_starty->push_back(track.Start().Y());
+      track_startz->push_back(track.Start().Z());
+      track_endx->push_back(track.End().X());
+      track_endy->push_back(track.End().Y());
+      track_endz->push_back(track.End().Z());
+
+      if (isSimulation){
+        art::FindMany< simb::MCParticle, anab::BackTrackerHitMatchingData > particlesPerHit(hitHandle, e, fHitTruthAssn);
+        std::pair< const simb::MCParticle*, double > mcpInformation = GetAssociatedMCParticle(hits, particlesPerHit);
+
+        const simb::MCParticle* mcpMatchedParticle = mcpInformation.first;
+        true_purity = mcpInformation.second;
+        
+        true_PDG = mcpMatchedParticle->PdgCode();
+
+      }
     }
-
-
   }
-  // this is probably fine
   else if (nSelectedTPCObjects == 0){
+    // this is probably fine
 
     std::cout << "[NuMuSelection] Event has " << nSelectedTPCObjects << std::endl;
     isUBXSecSelected = false;
@@ -286,7 +376,7 @@ void NuMuSelection1muNpAnalyzer::analyze(art::Event const & e)
     std::cout << "[NuMuSelection] Event has " << nSelectedTPCObjects << std::endl;
     mf::LogError(__PRETTY_FUNCTION__) << "[NuMuSelection] Event has " << nSelectedTPCObjects << std::endl;
     throw std::exception();
-  
+
   }
 
   ana_tree->Fill();
@@ -308,12 +398,94 @@ void NuMuSelection1muNpAnalyzer::endSubRun(art::SubRun const & sr)
 
 void NuMuSelection1muNpAnalyzer::initialiseAnalysisTree(TTree *tree, bool fIsData){
 
-  tree->Branch("run"    , &run);
-  tree->Branch("subrun" , &subrun);
-  tree->Branch("event"  , &event);
-  tree->Branch("isData" , &isData);
-  tree->Branch("isSimulation", &isSimulation);
-  tree->Branch("isUBXSecSelected", &isUBXSecSelected);
+  tree->Branch("run"              , &run);
+  tree->Branch("subrun"           , &subrun);
+  tree->Branch("event"            , &event);
+  tree->Branch("isData"           , &isData);
+  tree->Branch("isSimulation"     , &isSimulation);
+  tree->Branch("isUBXSecSelected" , &isUBXSecSelected);
+  tree->Branch("noBragg_fwd_mip"  , &noBragg_fwd_mip);
+  tree->Branch("bragg_fwd_mu"     , "std::vector<double>" , &bragg_fwd_mu);
+  tree->Branch("bragg_fwd_p"      , "std::vector<double>" , &bragg_fwd_p );
+  tree->Branch("bragg_fwd_pi"     , "std::vector<double>" , &bragg_fwd_pi);
+  tree->Branch("bragg_fwd_k"      , "std::vector<double>" , &bragg_fwd_k );
+  tree->Branch("bragg_bwd_mu"     , "std::vector<double>" , &bragg_bwd_mu);
+  tree->Branch("bragg_bwd_p"      , "std::vector<double>" , &bragg_bwd_p );
+  tree->Branch("bragg_bwd_pi"     , "std::vector<double>" , &bragg_bwd_pi);
+  tree->Branch("bragg_bwd_k"      , "std::vector<double>" , &bragg_bwd_k );
+  tree->Branch("track_length"     , "std::vector<double>" , &track_length   );
+  tree->Branch("track_theta"      , "std::vector<double>" , &track_theta    );
+  tree->Branch("track_costheta"   , "std::vector<double>" , &track_costheta );
+  tree->Branch("track_phi"        , "std::vector<double>" , &track_phi      );
+  tree->Branch("track_startx"     , "std::vector<double>" , &track_startx   );
+  tree->Branch("track_starty"     , "std::vector<double>" , &track_starty   );
+  tree->Branch("track_startz"     , "std::vector<double>" , &track_startz   );
+  tree->Branch("track_endx"       , "std::vector<double>" , &track_endx     );
+  tree->Branch("track_endy"       , "std::vector<double>" , &track_endy     );
+  tree->Branch("track_endz"       , "std::vector<double>" , &track_endz     );
+  tree->Branch("true_purity"      , &true_purity);
+  tree->Branch("true_PDG"         , &true_PDG);
+
+}
+
+void NuMuSelection1muNpAnalyzer::resizeVectors(){
+
+    noBragg_fwd_mip->resize(0);
+    bragg_fwd_mu->resize(0);
+    bragg_fwd_p->resize(0);
+    bragg_fwd_pi->resize(0);
+    bragg_fwd_k->resize(0);
+    bragg_bwd_mu->resize(0);
+    bragg_bwd_p->resize(0);
+    bragg_bwd_pi->resize(0);
+    bragg_bwd_k->resize(0);
+    track_length->resize(0);
+    track_theta->resize(0);
+    track_costheta->resize(0);
+    track_phi->resize(0);
+    track_startx->resize(0);
+    track_starty->resize(0);
+    track_startz->resize(0);
+    track_endx->resize(0);
+    track_endy->resize(0);
+    track_endz->resize(0);
+
+}
+
+std::pair< const simb::MCParticle*, double > NuMuSelection1muNpAnalyzer::GetAssociatedMCParticle(std::vector< art::Ptr< recob::Hit > > hits, art::FindMany<simb::MCParticle, anab::BackTrackerHitMatchingData> particlesPerHit)
+{
+
+  std::unordered_map<int,double> trkide;
+  double maxe=-1, tote=0;
+  simb::MCParticle const* maxp_me = NULL; //pointer for the particle match we will calculate
+
+  std::vector<simb::MCParticle const*> particle_vec;
+  std::vector<anab::BackTrackerHitMatchingData const*> match_vec;
+
+  //loop only over our hits
+  for(size_t i_h=0; i_h<hits.size(); ++i_h){
+
+    particle_vec.clear(); match_vec.clear();
+    particlesPerHit.get(hits[i_h].key(),particle_vec,match_vec);
+    //the .key() gives us the index in the original collection
+
+    //loop over particles
+    for(size_t i_p=0; i_p<particle_vec.size(); ++i_p){
+      trkide[ particle_vec[i_p]->TrackId() ] += match_vec[i_p]->energy; //store energy per track id
+      tote += match_vec[i_p]->energy; //calculate total energy deposited
+      if( trkide[ particle_vec[i_p]->TrackId() ] > maxe ){ //keep track of maximum
+        maxe = trkide[ particle_vec[i_p]->TrackId() ];
+        maxp_me = particle_vec[i_p];
+      }
+    }//end loop over particles per hit
+
+  }
+
+  std::pair< const simb::MCParticle* , double > returner;
+  returner.first  = maxp_me;
+  returner.second = maxe/tote;
+
+  return returner;
 
 }
 
