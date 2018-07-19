f20e32ea3897b7f9d6d249fed5faf4b008643482
Adam Lister <adamlister92@gmail.com>
2018-07-19 12:02:40 -0500

    make from local pc script

---
+++ Scripts/makeFromHome.sh
@@ -0,0 +1,2 @@
+g++ -o MakeDataSimulationComparisonPlots MakeDataSimulationComparisonPlots.cxx SelectionMaker.cxx EventCategoriser.cxx EfficiencyPurity.cxx AnalysisCuts.cxx `root-config --cflags --libs`
+
