800457d03ad79d946b96ba41c766de8b61895dff
Adam <a.lister1@lancaster.ac.uk>
2019-02-09 11:57:47 -0600

    final additions to script

--- Scripts/make_systematics_plot.C
+++ Scripts/make_systematics_plot.C
@@ -8,6 +8,23 @@ void make_systematics_plot(){
     else if (flux_or_xsec == 1)
         n_uni = 50;
 
+    TFile *fdirt = new TFile("/uboone/data/users/alister1/numuSelection/files/selected_events/selectedEvents_dirt.root", "read");
+    TTree* tdirt = (TTree*)fdirt->Get("simulation");
+    TH1D* hdirt = new TH1D("hdirt", ";Reconstructed neutrino energy (GeV);", 25, 0, 3);
+    tdirt->Draw("reconstructed_neutrino_energy_calib >> hdirt");
+    hdirt->Sumw2();
+    hdirt->Scale(4.89/168.796);
+    
+    TFile *foffbeam = new TFile("/uboone/data/users/alister1/numuSelection/files/selected_events/selectedEvents_offbeam.root", "read");
+    TTree* toffbeam = (TTree*)foffbeam->Get("offbeam");
+    TH1D* hoffbeam = new TH1D("hoffbeam", ";Reconstructed neutrino energy (GeV);", 25, 0, 3);
+    toffbeam->Draw("reconstructed_neutrino_energy_calib >> hoffbeam");
+    hoffbeam->Sumw2();
+    hoffbeam->Scale(10905211./77219137.);
+
+    std::cout << "dirt integral: " << hdirt->Integral() << std::endl;
+    std::cout << "offbeam integral: " << hoffbeam->Integral() << std::endl;
+
     TFile *_file0 = new TFile("/uboone/data/users/alister1/SBNFit_files/filtered_events_mc_numu_sbnfit.root");
     TTree *t_cc0pinp = (TTree*)_file0->Get("numu_sbnfit_cc0pinp");
     TTree *t_ccother = (TTree*)_file0->Get("numu_sbnfit_ccother");
@@ -92,6 +109,23 @@ void make_systematics_plot(){
     t_nue->Draw("reconstructed_neutrino_energy_calib >> h_nue_cv");
     t_anumu->Draw("reconstructed_neutrino_energy_calib >> h_anumu_cv");
 
+    h_cc0pinp_cv->Sumw2();
+    h_ccother_cv->Sumw2();
+    h_oofv_cv->Sumw2();
+    h_cosmic_cv->Sumw2();
+    h_nc_cv->Sumw2();
+    h_mixed_cv->Sumw2();
+    h_nue_cv->Sumw2();
+    h_anumu_cv->Sumw2();
+    h_cc0pinp_cv->Scale(4.89/185.731);
+    h_ccother_cv->Scale(4.89/185.731);
+    h_oofv_cv->Scale(4.89/185.731);
+    h_cosmic_cv->Scale(4.89/185.731);
+    h_nc_cv->Scale(4.89/185.731);
+    h_mixed_cv->Scale(4.89/185.731);
+    h_nue_cv->Scale(4.89/185.731);
+    h_anumu_cv->Scale(4.89/185.731);
+
     TH1D *tot_cv = new TH1D("tot_cv", ";Reconstructed neutrino energy (GeV);", 25, 0, 3);
 
     tot_cv->Add(h_cc0pinp_cv);
@@ -102,6 +136,8 @@ void make_systematics_plot(){
     tot_cv->Add(h_mixed_cv);
     tot_cv->Add(h_nue_cv);
     tot_cv->Add(h_anumu_cv);
+    tot_cv->Add(hdirt);
+    tot_cv->Add(hoffbeam);
 
     //
     // alt universes
@@ -185,16 +221,53 @@ void make_systematics_plot(){
     
         //loop histogram
         for (int i_hist = 0; i_hist < histogram_vector.size()-1; i_hist++){
+            histogram_vector.at(i_hist).at(i)->Sumw2();
+            histogram_vector.at(i_hist).at(i)->Scale(4.89/185.731);
             histogram_vector.at(histogram_vector.size()-1).at(i)->Add(histogram_vector.at(i_hist).at(i));
         }
+            
+        histogram_vector.at(histogram_vector.size()-1).at(i)->Add(hdirt);
+        histogram_vector.at(histogram_vector.size()-1).at(i)->Add(hoffbeam);
 
     }
+
+
+    TH2D* h_tot_2d = new TH2D("h_tot_2d", ";Reconstructed neutrino energy (GeV);", 25, 0, 3, 25, 0, histogram_vector.at(histogram_vector.size()-1).at(0)->GetMaximum()*2);
+
+    if (flux_or_xsec == 0)
+        h_tot_2d->SetBins(25, 0, 3, 60, 0, histogram_vector.at(histogram_vector.size()-1).at(0)->GetMaximum()*1.5);
+
+    if (flux_or_xsec == 1)
+        h_tot_2d->SetBins(25, 0, 3, 30, 0, histogram_vector.at(histogram_vector.size()-1).at(0)->GetMaximum()*2);
 
     for (int i = 0; i < n_uni; i++){
-        histogram_vector.at(histogram_vector.size()-1).at(i)->SetLineColor(kGreen+1);
-        histogram_vector.at(histogram_vector.size()-1).at(i)->Draw("same");
+        // get histogram
+        TH1D* this_hist = histogram_vector.at(histogram_vector.size()-1).at(i);
+        // loop bing
+        for (int i_b = 0; i_b < this_hist->GetNbinsX(); i_b++){
+            float binCenter = this_hist->GetBinCenter(i_b);
+            float binContent = this_hist->GetBinContent(i_b);
+            h_tot_2d->Fill(binCenter, binContent);
+        }
     }
-    tot_cv->DrawClone("same");
+    TCanvas *c1 = new TCanvas("c1", "c1", 600, 400);
+    if (flux_or_xsec == 0){
+        gStyle->SetPalette(kAvocado);
+        tot_cv->SetLineColor(kGreen+1);
+    }
+    if (flux_or_xsec == 1){
+        gStyle->SetPalette(kCool);
+        tot_cv->SetLineColor(kBlue+1);
+    }
+    h_tot_2d->Draw("col");
+    tot_cv->SetLineWidth(2);
+    tot_cv->Draw("same");
 
+    if (flux_or_xsec == 0){
+        c1->SaveAs("flux_systematics.pdf");
+    }
+    if (flux_or_xsec == 1){
+        c1->SaveAs("xsec_systematics.pdf");
+    }
 
 }
